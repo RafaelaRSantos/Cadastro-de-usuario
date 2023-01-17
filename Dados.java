d70cb0fd95cd5e596e43a8946d1ce98e96dcda21
Rafaela ramos dos santos <112700179+RafaelaRSantos@users.noreply.github.com>
2023-01-17 10:35:25 -0300

    Add files via upload

---
+++ Dados.java
@@ -0,0 +1,63 @@
+
+import java.util.ArrayList;
+
+//CLASSE QUE GUARDA AS VARIAVEIS DOS OBJETOS...
+public class Dados {
+ 
+private String nome;
+
+private int dataNascimento;
+
+private String endereco;
+
+private String cidade;
+ 
+
+public Dados(){
+
+
+}
+
+
+public String getNome() {
+	return nome;
+}
+
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+
+public int getDataNascimento() {
+	return dataNascimento;
+}
+
+
+public void setDataNascimento(int dataNascimento) {
+	this.dataNascimento = dataNascimento;
+}
+
+
+public String getEndereco() {
+	return endereco;
+}
+
+
+public void setEndereco(String endereco) {
+	this.endereco = endereco;
+}
+
+
+public String getCidade() {
+	return cidade;
+}
+
+
+public void setCidade(String cidade) {
+	this.cidade = cidade;
+}
+
+
+}
+
